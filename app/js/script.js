996a63b3e7dc5b0678de8a07033ea9520d9c4e66
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2021-06-27 11:52:50 +0800

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('#button');
-const title = document.querySelectorAll('.title--text');
+const title = document.querySelector('.title--text');
+const overviewText = document.querySelector('.overview--text')
 const bigBox = document.querySelectorAll('.social-big');
 const smallBox = document.querySelectorAll('.overview-box')
 const totalFollowers = document.querySelectorAll('.total-followers');
@@ -11,9 +12,8 @@ button.addEventListener('change', e => {
         heading.style.backgroundColor = 'hsl(225, 100%, 98%)';
         body.style.backgroundColor = 'hsl(0, 0%, 100%)';
         
-        title.forEach(e => {
-            e.style.color = 'hsl(230, 17%, 14%)';
-        }
+        title.style.color = 'hsl(230, 17%, 14%)';
+        overviewText.style.color = 'hsl(230, 17%, 14%)';
         bigBox.forEach(e => {
             e.style.backgroundColor = 'hsl(227, 47%, 96%)';
         })
@@ -31,9 +31,8 @@ button.addEventListener('change', e => {
     } else {
         heading.style.backgroundColor = 'hsl(232, 19%, 15%)';
         body.style.backgroundColor = 'hsl(230, 17%, 14%)';
-        title.forEach(e => {
-            e.style.color = 'hsl(0, 0%, 100%)';                 
-        })
+        title.style.color = 'hsl(0, 0%, 100%)';                 
+        overviewText.style.color = 'hsl(0, 0%, 100%)';
         bigBox.forEach(e => {
             e.style.backgroundColor = 'hsl(228, 28%, 20%)';
         })
